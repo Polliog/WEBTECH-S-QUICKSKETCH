4f6786aeaf5d7ec3159d445601799c5baf759d4d
Polliog <polliogiuseppe@live.it>
2026-07-01 08:38:47 +0200

    Date in italiano: registra locale it-IT
    Le date usavano il locale predefinito en-US ('1 Jul', '1 July 2026').
    Registrato il locale 'it' e impostato LOCALE_ID a 'it-IT' cosi il DatePipe
    mostra '1 lug' e '1 luglio 2026'.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,19 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth.interceptor';
+
+// Date, numeri e valute in italiano (DatePipe, DecimalPipe, ...)
+registerLocaleData(localeIt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +21,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LOCALE_ID, useValue: 'it-IT' },
   ],
 };
